de88bd134af0781d5537f453c828c8b00613c620
Rafey <89015458+RafeyGx@users.noreply.github.com>
2024-03-19 01:09:24 +0000

    lol

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,21 @@ import {fadeIn} from '../variants'
 
 const Home = () => {
 
-	return <div className=' bg-primary/60 h-full '>
+	return ( <div className=' bg-primary/60 h-full '>
 		{/* text */}
 		<div>
 		 <div>
 			{/*title*/}
-			<h1>Transforming ideas <br/> Into <span>Digital reality</span></h1>
+			<h1 className='h1'>
+				Transforming ideas <br/> Into <span className='text-accent'>Digital reality</span>
+			</h1>
+			{/* subtitle */}
+			<p className ='max-w-sm xl:max-w-xl mx-auto xl:mx-0'> Lorem ipsum dolor sit amet orci aliquam.Lorem ipsum dolor sit amet orci aliquam.Lorem ipsum dolor sit amet orci aliquam. </p>
 		</div>
 		 </div>
 		{/* image */}
 		<div>image</div>
-	</div>;
+	</div>
 	);
 };
 
