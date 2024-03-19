9f3a0dd7a2e1b8929e9eaa108f659d1cca63698f
Rafey <89015458+RafeyGx@users.noreply.github.com>
2024-03-19 00:32:50 +0000

    completed half of components

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,47 @@
+import Link from "next/link";
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiDribbbleLine,
+  RiBehanceLine,
+  RiPinterestLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-4 text-lg">
+      <Link
+        href={"https://t.me/rafeygx"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiYoutubeLine />
+      </Link>
+      <Link
+        href={"https://t.me/rafeygx"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://t.me/rafeygx"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiBehanceLine />
+      </Link>
+      <Link
+        href={"https://t.me/rafeygx"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiPinterestLine />
+      </Link>
+      <Link
+        href={"https://t.me/rafeygx"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiDribbbleLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
