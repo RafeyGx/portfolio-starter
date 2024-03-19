f9ab3b3bdfe0a6565ec52725e960ff34b19d6827
Rafey <89015458+RafeyGx@users.noreply.github.com>
2024-03-19 00:01:01 +0000

    navbar
    made changes to navbar

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,13 @@
+// next image
+
+import Image from "next/Image";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+	return (
+		<div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl-[400px] opacity-50">
+			<image src="/top-left-img.png" width={400} height={400} />
+		</div>
+	);
 };
 
 export default TopLeftImg;
--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 // components
 
-import Layout from '../components/Layout'
-import Transition from '../components/Transition'
-
+import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 
 // router
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-// framer motion 
-import { AnimatePresence, motion } from 'framer-motion'
+// framer motion
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <Layout>
-      <AnimatePresence mode='wait'>
-        <motion.div key={router. route} className='h-full'>
-          <Transition />
-          <Component {...pageProps} />;
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
-  );
+	const router = useRouter();
+	return (
+		<Layout>
+			<AnimatePresence mode="wait">
+				<motion.div key={router.route} className="h-full">
+					<Transition />
+					<Component {...pageProps} />;
+				</motion.div>
+			</AnimatePresence>
+		</Layout>
+	);
 }
 
 export default MyApp;
